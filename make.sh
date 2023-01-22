89fa95c5f933f890b2f338a363eea8eb568298db
Rafał Lewandowski <rafal_lewandowski@outlook.com>
2023-01-22 18:48:41 +0100

    Use the 2.0 tag for the texlive image

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm --interactive --workdir "/doc" --volume "$PWD":/doc thomasweise/docker-texlive-full make "$@"
+docker run --rm --interactive --workdir "/doc" --volume "$PWD":/doc thomasweise/docker-texlive-full:2.0 make "$@"
